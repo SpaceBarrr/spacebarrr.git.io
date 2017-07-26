810799cd31cf2e643a3fdb2bd40d02c8a3f4988a
SpaceBarrr <SpaceBarrr@users.noreply.github.com>
2017-07-26 20:32:36 +0800

    suicide

---
+++ main.js
@@ -0,0 +1,34 @@
+// more stolen code ayy lmao
+
+//$(function()
+//    {
+//        var bgMusic = $('#audio-bg')[0],
+//            playing = true;
+//
+//        bgMusic.addEventListener('ended', function() {
+//            this.currentTime = 0;
+//            if (playing) {
+//                this.play();
+//            }
+//        }, false);
+//
+//        bgMusic.play();
+//
+//        $('#toggle').click(function() {
+//            if (!bgMusic.paused) {
+//                bgMusic.pause();
+//                playing = false;
+//                $(this).css({backgroundPosition: '0 -21px'})
+//            } else {
+//                bgMusic.play();
+//                playing = true;
+//               $(this).css({backgroundPosition: '0 0'})
+//            }
+//        });
+//    });
+
+if (confirm("Click OK to confirm you are not Nicholas Franz!") == true) {
+    return;
+} else {
+    txt = "fuck off nic";
+}
